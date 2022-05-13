89b319caa0b3f1686812d629b74a84fb1583e5c2
Carlos Biagolini-Jr <c.biagolini@gmail.com>
2022-05-13 15:16:10 -0300

    New itens

--- src/app/flexbox/flexbox-routing.module.ts
+++ src/app/flexbox/flexbox-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlignContentComponent } from './components/align-content/align-content.component';
+import { AlignItemsComponent } from './components/align-items/align-items.component';
 
 import { BasicComponent } from './components/basic/basic.component';
 import { DirectionComponent } from './components/direction/direction.component';
+import { FlexBasisComponent } from './components/flex-basis/flex-basis.component';
+import { FlexGrowComponent } from './components/flex-grow/flex-grow.component';
+import { FlexShrinkComponent } from './components/flex-shrink/flex-shrink.component';
 import { FlowComponent } from './components/flow/flow.component';
+import { JustifyContentComponent } from './components/justify-content/justify-content.component';
 import { ReferencesComponent } from './components/references/references.component';
 import { WrapComponent } from './components/wrap/wrap.component';
 
@@ -12,6 +18,12 @@ const routes: Routes = [
   { path: 'direction', component: DirectionComponent },
   { path: 'wrap', component: WrapComponent },
   { path: 'flow', component: FlowComponent },
+  { path: 'justify-content', component: JustifyContentComponent },
+  { path: 'align-items', component: AlignItemsComponent }, 
+  { path: 'align-content', component: AlignContentComponent }, 
+  { path: 'flex-grow', component: FlexGrowComponent }, 
+  { path: 'flex-basis', component: FlexBasisComponent }, 
+  { path: 'flex-shrink', component: FlexShrinkComponent },   
   { path: 'reference', component: ReferencesComponent },
 ];
 
--- src/app/flexbox/flexbox.module.ts
+++ src/app/flexbox/flexbox.module.ts
@@ -9,6 +9,11 @@ import { ReferencesComponent } from './components/references/references.componen
 import { WrapComponent } from './components/wrap/wrap.component';
 import { FlexboxRoutingModule } from './flexbox-routing.module';
 import { FlowComponent } from './components/flow/flow.component';
+import { JustifyContentComponent } from './components/justify-content/justify-content.component';
+import { AlignContentComponent } from './components/align-content/align-content.component';
+import { FlexGrowComponent } from './components/flex-grow/flex-grow.component';
+import { FlexBasisComponent } from './components/flex-basis/flex-basis.component';
+import { FlexShrinkComponent } from './components/flex-shrink/flex-shrink.component';
 
 
 @NgModule({
@@ -18,7 +23,12 @@ import { FlowComponent } from './components/flow/flow.component';
     BasicComponent,
     ReferencesComponent,
     WrapComponent,
-    FlowComponent
+    FlowComponent,
+    JustifyContentComponent,
+    AlignContentComponent,
+    FlexGrowComponent,
+    FlexBasisComponent,
+    FlexShrinkComponent
   ],
   imports: [
     CommonModule,
